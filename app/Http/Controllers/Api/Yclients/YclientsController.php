525f24fcf87e3275f0ddb69b31501e10bf24553c
IceRDP2 <radicaldreamers.ff@gmail.com>
2024-05-08 22:00:54 +0300

    добавили new client days

--- app/Http/Controllers/Api/Yclients/YclientsController.php
+++ app/Http/Controllers/Api/Yclients/YclientsController.php
@@ -12,25 +12,30 @@ use App\Utils\Telnums;
 
 class YclientsController extends Controller
 {
-    function getVisitsMonthAgo() {
+    private function getClientsVisits($clients_key)
+    {
         try {
-            $date = Carbon::now()->subDay(30)->format('Y-m-d');
-
-            $params = [
-                "start_date" => $date,
-                "end_date"   => $date,
-            ];
-
-            $client = new YclientsService($params);
+            $client = new YclientsService();
 
             $partners = Telnums::getTelnumsList();
 
             $result = [];
 
             foreach ($partners as $key => $partner) {
-                if (!array_key_exists("send_visits", $partner) || $partner["send_visits"] !== true) continue;
+                if (!array_key_exists($clients_key, $partner) ||
+                    !is_numeric($partner[$clients_key]) ||
+                    $partner[$clients_key] <= 0
+                ) continue;
 
-                $client->setCompanyId($partner["company_id"]);
+                $client_days = $partner[$clients_key];
+
+                $date = Carbon::now()->subDay($client_days)->format('Y-m-d');
+
+                $client->updateParams([
+                    "company_id" => $partner["company_id"],
+                    "start_date" => $date,
+                    "end_date"   => $date,
+                ]);
 
                 $records = $client->getRecordsList();
 
@@ -47,11 +52,10 @@ class YclientsController extends Controller
                 $visits = $client->getVisitedForPeriod($recordsIds);
 
                 foreach ($visits as $visit) {
-                    if (!array_key_exists("last_visit_date", $visit) ||
-                        Carbon::parse($visit["last_visit_date"])->diffInDays($date, false) < 0
-                    ) {
-                        continue;
-                    }
+                    if (!array_key_exists("last_visit_date", $visit) &&
+                        Carbon::parse($visit["last_visit_date"])->diffInDays($date, false) < 0 &&
+                        ($clients_key === "new_client_days" && $visit["visits_count"] != 1)
+                    ) continue;
 
                     $id = $visit["id"];
 
@@ -59,24 +63,31 @@ class YclientsController extends Controller
                     $company_id = $partner["company_id"];
 
                     $result[$tg_chat_id][$company_id][] = [
-                        "id"              => $id,
-                        "name"            => $visit["name"],
-                        "phone"           => $visit["phone"],
-                        "visits_count"    => $visit["visits_count"],
-                        "services"        => array_key_exists($id, $records)
+                        "id"           => $id,
+                        "name"         => $visit["name"],
+                        "phone"        => $visit["phone"],
+                        "visits_count" => $visit["visits_count"],
+                        "services"     => array_key_exists($id, $records)
                             ? $records[$id]["services"]
                             : [],
-                        "partner_name"    => $partner["name"]
+                        "partner_name" => $partner["name"],
+                        "days"         => $client_days
                     ];
                 }
-
             }
 
             return $result;
-
         } catch (Throwable $e) {
             report($e->getMessage());
             return false;
         }
+    }
+
+    public function getLostClients() {
+        return $this->getClientsVisits("lost_client_days");
+    }
+
+    public function getNewClients() {
+        return $this->getClientsVisits("new_client_days");
     }
 }
