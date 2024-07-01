8340c179755b49bdbbacee9f9deacf07f0d1297b
IceRDP2 <radicaldreamers.ff@gmail.com>
2024-07-01 23:10:12 +0300

    update post image size

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -6,7 +6,7 @@
     </header>
     @if($post->image)
         <span class="image main" style="display: flex; justify-content: center;">
-            <img style="width: auto; max-width: 100%;" src="{{ asset($post->image) }}" alt="">
+            <img style="max-width: 600px;" src="{{ asset($post->image) }}" alt="">
         </span>
     @endif
     <div class="post-description"> {!! $post->description !!} </div>
