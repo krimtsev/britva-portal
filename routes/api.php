5e917baff12e2a9dcf4bb56e7485598e2a10a599
IceRDP2 <radicaldreamers.ff@gmail.com>
2024-09-28 21:30:56 +0300

    fix blacklist api link

--- routes/api.php
+++ routes/api.php
@@ -11,8 +11,7 @@ Route::group(['middleware' => ['api_token']], function () {
     Route::get('/mango', [MangoController::class, 'index']);
     Route::get('/mango/test', [MangoController::class, 'test']);
     Route::get('/mango/statistics', [MangoController::class, 'getStatisticsForPastDay']);
-    Route::get('/mango/blacklist', [MangoController::class, 'blacklist']);
-    Route::get('/mango/check-number/{number}', [MangoController::class, 'checkNumberInBlacklist']);
+    Route::get('/mango/blacklist', [MangoController::class, 'updateBlacklist']);
 
     Route::get('/lost-clients', [ClientsVisitsController::class, 'getLostClients']);
     Route::get('/new-clients', [ClientsVisitsController::class, 'getNewClients']);
