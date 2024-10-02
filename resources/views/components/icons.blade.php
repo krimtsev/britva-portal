4103ffb5c060629b96bd3f1421ff95a22b3fec30
IceRDP2 <radicaldreamers.ff@gmail.com>
2024-10-02 18:33:07 +0300

    fix min icons size

--- resources/views/components/icons.blade.php
+++ resources/views/components/icons.blade.php
@@ -1,3 +1,3 @@
-<div style="width: {{$size}}px; height: {{$size}}px">
+<div style="width: {{$size}}px; height: {{$size}}px; min-width: {{$size}}px; min-height: {{$size}}px;">
     <img class="icon-files" src="{{ $src }}"  alt="file" />
 </div>
